92f5f33583157e4159d81c723ecfbc655d7606e6
terfno <sueda.takahito@gmail.com>
2019-04-12 09:35:13 +0900

    add 1 and 1_2

---
+++ chapter1_2/chapter1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chapter1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.WriteLine("Hello World!" + "Takahito Sueda");
+        }
+    }
+}
