9c499776be256f1bb8c902a77fd47d4993e90158
terfno <sueda.takahito@gmail.com>
2019-04-12 09:51:18 +0900

    fix img name, and add 1.4

---
+++ chapter1_3/chapter1_3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace chapter1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 図1.5
+            int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6;
+            Console.WriteLine(a + b);
+            Console.WriteLine(b - c);
+            Console.WriteLine(c * d);
+            Console.WriteLine(d / e);
+            Console.WriteLine(e % f);
+
+            // 図1.6
+            double de = 5.0;
+            Console.WriteLine(d / (int)de);
+            Console.WriteLine(d / de);
+            //Console.WriteLine((bool)42);
+
+            Console.WriteLine("Hello World!" + "Takahito Sueda");
+            Console.ReadKey();
+        }
+    }
+}
