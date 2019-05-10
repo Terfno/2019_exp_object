44dc08e13f6ddd879346491fa3034153375fea3f
terfno <sueda.takahito@gmail.com>
2019-05-10 09:14:17 +0900

    update

---
+++ chapter4_1/chapter4_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace chapter4_1
+{
+    class Program
+    {
+        static bool Containe (int[] a, int aLen, int b)
+        {
+            for(int i = 0; i < aLen; i++)
+            {
+                if (a[i] == b)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void IntersectAndPrint (int[] a, int[] b)
+        {
+            int[] dup = new int[a.Length];
+
+            int dupCnt = 0;
+
+            for(int j = 0; j < a.Length; j++)
+            {
+                if (Containe(dup, dupCnt, a[j]))
+                {
+                    continue;
+                }
+
+                for (int i = 0; i < dupCnt; i++)
+                {
+                    // 略
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a1 = new int[5] { 1, 2, 3, 4, 5 };
+            int[] a2 = new int[5] { 3, 1, 5, 10, 11 };
+
+            IntersectAndPrint(a1, a2);
+
+            Console.ReadKey();
+        }
+    }
+}
